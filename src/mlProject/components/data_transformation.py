27f46c696976d8e8e02033a8ef74d847b263ee3c
Ashish-Ghoshal <ghoshal.ashish29@gmail.com>
2024-05-21 19:33:26 +0530

    going to try deleting artifacts

--- src/mlProject/components/data_transformation.py
+++ src/mlProject/components/data_transformation.py
@@ -22,10 +22,12 @@ class DataTransformation:
         # Would have to add outlier handling and SCALING here
         
         
-        # Add PCA HERE
+        
         
         # Split the data into training and test sets. (0.75, 0.25) split.
         train, test = train_test_split(data)
+        
+        # Add PCA HERE
 
         train.to_csv(os.path.join(self.config.root_dir, "train.csv"),index = False)
         test.to_csv(os.path.join(self.config.root_dir, "test.csv"),index = False)
