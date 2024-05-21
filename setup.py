8c228a37c8bfc4ddc76293804afbe9cacf3937e3
Ashish-Ghoshal <ghoshal.ashish29@gmail.com>
2024-05-21 08:29:54 +0530

    modified setup

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "End-to-end-ML-Project-Implementation"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "ML_Proj_default"
+AUTHOR_USER_NAME = "ashish"
 SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "ghoshal.ashish29@gmail.com"
 
 
 setuptools.setup(
